45d55923fa923610ee25931cb0314d1a3301316e
Suresh Kansujiya <sureshkansujiya@gmail.com>
2020-01-23 13:15:27 +0530

    Update builder.swift

--- source/creational/builder.swift
+++ source/creational/builder.swift
@@ -7,47 +7,103 @@ An external class controls the construction algorithm.
 
 ### Example
 */
-final class DeathStarBuilder {
+import Foundation
 
-    var x: Double?
-    var y: Double?
-    var z: Double?
+// Models
+enum DishCategory: Int {
+    case firstCourses, mainCourses, garnishes, drinks
+}
 
-    typealias BuilderClosure = (DeathStarBuilder) -> ()
+struct Dish {
+    var name: String
+    var price: Float
+}
 
-    init(buildClosure: BuilderClosure) {
-        buildClosure(self)
+struct OrderItem {
+    var dish: Dish
+    var count: Int
+}
+
+struct Order {
+    var firstCourses: [OrderItem] = []
+    var mainCourses: [OrderItem] = []
+    var garnishes: [OrderItem] = []
+    var drinks: [OrderItem] = []
+    
+    var price: Float {
+        let items = firstCourses + mainCourses + garnishes + drinks
+        return items.reduce(Float(0), { $0 + $1.dish.price * Float($1.count) })
     }
 }
 
-struct DeathStar : CustomStringConvertible {
-
-    let x: Double
-    let y: Double
-    let z: Double
-
-    init?(builder: DeathStarBuilder) {
-
-        if let x = builder.x, let y = builder.y, let z = builder.z {
-            self.x = x
-            self.y = y
-            self.z = z
-        } else {
-            return nil
+// Builder
+class OrderBuilder {
+    private var order: Order?
+    
+    func reset() {
+        order = Order()
+    }
+    
+    func setFirstCourse(_ dish: Dish) {
+        set(dish, at: order?.firstCourses, withCategory: .firstCourses)
+    }
+    
+    func setMainCourse(_ dish: Dish) {
+        set(dish, at: order?.mainCourses, withCategory: .mainCourses)
+    }
+    
+    func setGarnish(_ dish: Dish) {
+        set(dish, at: order?.garnishes, withCategory: .garnishes)
+    }
+    
+    func setDrink(_ dish: Dish) {
+        set(dish, at: order?.drinks, withCategory: .drinks)
+    }
+    
+    func getResult() -> Order? {
+        return order ?? nil
+    }
+    
+    private func set(_ dish: Dish, at orderCategory: [OrderItem]?, withCategory dishCategory: DishCategory) {
+        guard let orderCategory = orderCategory else {
+            return
+        }
+        
+        var item: OrderItem! = orderCategory.filter( { $0.dish.name == dish.name } ).first
+        
+        guard item == nil else {
+            item.count += 1
+            return
+        }
+        
+        item = OrderItem(dish: dish, count: 1)
+        
+        switch dishCategory {
+        case .firstCourses:
+            order?.firstCourses.append(item)
+        case .mainCourses:
+            order?.mainCourses.append(item)
+        case .garnishes:
+            order?.garnishes.append(item)
+        case .drinks:
+            order?.drinks.append(item)
         }
     }
-
-    var description:String {
-        return "Death Star at (x:\(x) y:\(y) z:\(z))"
-    }
-}
-/*:
-### Usage
-*/
-let empire = DeathStarBuilder { builder in
-    builder.x = 0.1
-    builder.y = 0.2
-    builder.z = 0.3
 }
 
-let deathStar = DeathStar(builder:empire)
+// Usage
+let steak = Dish(name: "Steak", price: 2.30)
+let chips = Dish(name: "Chips", price: 1.20)
+let coffee = Dish(name: "Coffee", price: 0.80)
+
+let builder = OrderBuilder()
+builder.reset()
+builder.setMainCourse(steak)
+builder.setGarnish(chips)
+builder.setDrink(coffee)
+
+let order = builder.getResult()
+order?.price
+
+// Result:
+// 4.30
